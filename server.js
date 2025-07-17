4865824a669e0eb7ab2394002a06830ec6f585aa
Ashutosh Sharma <mr.proashutosh@gmail.com>
2025-07-17 11:17:05 +0530

    bug fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT= process.env.PORT || 5000;
 
 //middleware
 app.use(cors({
-    origin: 'https://myleaderboardapp.netlify.app/', 
+    origin: 'https://myleaderboardapp.netlify.app', 
     methods: ['GET', 'POST','PUT'],
     credentials: true
 }));
